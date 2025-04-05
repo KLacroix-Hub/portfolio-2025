c9b0bcca5e5c2275ead289186ae3b5b0f698927e
KEVIN LACROIX <kevin.lacroix@hetic.net>
2025-04-05 15:58:18 +0200

    add: header nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,42 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import node from "@astrojs/node";
+import { fileURLToPath } from "url";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "http://localhost:4321/",
+  base: "/",
+  integrations: [],
+  output: "server",
+
+  adapter: node({
+    mode: "standalone",
+  }),
+  vite: {
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: [
+            "mixed-decls",
+            "color-functions",
+            "global-builtin",
+            "import",
+            "legacy-js-api",
+          ],
+          additionalData: `
+            @import '@/styles/_breakpoints.scss';
+            @import '@/styles/_variables.scss';
+            @import '@/styles/_fonts.scss'; 
+          `,
+        },
+      },
+    },
+  },
+});
